7dbf9e495a4b392a9c48d99e6f901900ccac1e51
quitsv <axellsilvano29@gmail.com>
2022-03-31 20:23:19 +0700

    changed sql settings

--- main.go
+++ main.go
@@ -2,19 +2,20 @@ package main
 
 import (
 	"apiTools/controllers"
-  "time"
+	"time"
+
 	"github.com/go-co-op/gocron"
 )
 
 func main() {
-  
-  s := gocron.NewScheduler(time.UTC)
+
+	s := gocron.NewScheduler(time.UTC)
 	//goMail
 	message := []byte("Testing masuk 123")
 	to := controllers.GetRecipient(1).Email
 	controllers.SendEmail(to, message)
 
 	//goRoutine
-  s.Every(5).Second().Do(func () {controllers.SendAsync()})
-  s.StartBlocking()
+	s.Every(5).Second().Do(func() { controllers.SendAsync() })
+	s.StartBlocking()
 }
