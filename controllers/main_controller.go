2265e0c57b6042ace858eb7134a6f5f906bd3042
MichaelLeonardo <michaelleonardo69@gmail.com>
2022-03-31 18:04:06 +0700

    <Commit Go email Function lebih simple edition>

--- controllers/main_controller.go
+++ controllers/main_controller.go
@@ -2,47 +2,25 @@ package controllers
 
 import (
 	"log"
-
-	mail "github.com/xhit/go-simple-mail/v2"
+	"net/smtp"
 )
 
 func SendEmail(emailPenerima string) {
-	var htmlBody = `
-	<html>
-	<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Hello, World</title>
-	</head>
-	<body>
-	<p>This is an email using Go</p>
-	</body>
-	`
-	server := mail.NewSMTPClient()
-	server.Host = "smtp.gmail.com"
-	server.Port = 587
-	server.Username = "if-20026@students.ithb.ac.id"
-	server.Password = "g"
-	server.Encryption = mail.EncryptionTLS
+	// Configuration
+	from := "stevianianggila60@gmail.com"
+	password := "NakNik919"
+	to := []string{"if-20026@students.ithb.ac.id"}
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
 
-	smtpClient, err := server.Connect()
+	message := []byte("My super secret message.")
+
+	// Create authentication
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Send actual message
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// Create email
-	email := mail.NewMSG()
-	email.SetFrom("From Me <if-20026@students.ithb.ac.id>")
-	email.AddTo("michaelleonardo69@gmail.com")
-	// email.AddCc("another_you@example.com")
-	email.SetSubject("New Go Email")
-
-	email.SetBody(mail.TextHTML, htmlBody)
-	// email.AddAttachment("super_cool_file.png")
-
-	// Send email
-	err = email.Send(smtpClient)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 }
