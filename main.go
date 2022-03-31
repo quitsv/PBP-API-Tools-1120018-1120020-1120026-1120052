9e1392e401b2873d8959a43c30dda3cd6b01d59a
MichaelLeonardo <michaelleonardo69@gmail.com>
2022-03-31 13:28:47 +0700

    <Commit Go email2>

--- main.go
+++ main.go
@@ -1,5 +1,48 @@
 package main
 
+import (
+	"log"
+
+	mail "github.com/xhit/go-simple-mail/v2"
+)
+
+var htmlBody = `
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   <title>Hello, World</title>
+</head>
+<body>
+   <p>This is an email using Go</p>
+</body>
+`
+
 func main() {
-	println("Hello, world.")
+	server := mail.NewSMTPClient()
+	server.Host = "smtp.gmail.com"
+	server.Port = 587
+	server.Username = "if-20026@students.ithb.ac.id"
+	server.Password = "g"
+	server.Encryption = mail.EncryptionTLS
+
+	smtpClient, err := server.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create email
+	email := mail.NewMSG()
+	email.SetFrom("From Me <if-20026@students.ithb.ac.id>")
+	email.AddTo("michaelleonardo69@gmail.com")
+	// email.AddCc("another_you@example.com")
+	email.SetSubject("New Go Email")
+
+	email.SetBody(mail.TextHTML, htmlBody)
+	// email.AddAttachment("super_cool_file.png")
+
+	// Send email
+	err = email.Send(smtpClient)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
